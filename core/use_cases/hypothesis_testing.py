30e7f4efaa179772aa44560fcf6f27d238c77f54
ariariqlubis <ariqf.lubis@gmail.com>
2024-12-17 23:57:59 +0700

    feat: Init HypothesisTesting

---
+++ core/use_cases/hypothesis_testing.py
@@ -0,0 +1,9 @@
+from utils.normality_check import check_normality
+from core.entities.test_result import TestResult
+
+class HypothesisTesting:
+    def __init__(self):
+        pass
+
+    def run_test(self, group1, group2):
+        pass
