8808d34e16f6a520768f951c345bfcf1e3544eb6
ariariqlubis <ariqf.lubis@gmail.com>
2024-12-18 10:17:10 +0700

    feat: update hypothesis.py

--- core/entities/hypothesis.py
+++ core/entities/hypothesis.py
@@ -1,4 +1,18 @@
+from typing import Union
+import pandas as pd
+
 class Hypothesis:
-    def __init__(self, name: str, data: list):
-        self.name = name
-        self.data = data
+    """
+    
+    """
+    def __init__(self, data: Union[pd.DataFrame, list], alpha: float = 0.05, columns: list = None, is_categorical: bool = False):
+        self.alpha = alpha
+        self.is_categorical = is_categorical
+
+        if isinstance(data, pd.DataFrame):
+            if not columns:
+                raise ValueError("You must specify columns for comparison in a DataFrame.")
+            self.data = [data[col].dropna().tolist() for col in columns]
+        else:
+            self.data = data
+
