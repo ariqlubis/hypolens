28631cd57b3e7b1aab0462b2a0c8fb6ebb29db95
ariariqlubis <ariqf.lubis@gmail.com>
2024-12-18 11:59:04 +0700

    removing hypothesis_tester

--- core/interfaces/hypothesis_tester.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from core.entities.hypothesis import Hypothesis
-
-class HypothesisTester(ABC):
-    @abstractmethod
-    def test(self, hypothesis: Hypothesis):
-        pass
--- core/use_cases/hypothesis_testing.py
+++ core/use_cases/hypothesis_testing.py
@@ -1,9 +1,11 @@
 from utils.normality_check import check_normality
 from core.entities.test_result import TestResult
+from core.interfaces.hypothesis_tester import HypothesisTester
 
 class HypothesisTesting:
-    def __init__(self):
-        pass
+    def __init__(self, tester: HyphotesisTester):
+        self.tester = tester
 
-    def run_test(self, group1, group2):
-        pass
+    def run_test(self, hypothesis):
+        if check_normality(hypothesis.data[0]):
+            pass
