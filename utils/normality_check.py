607fed833040eec88b09f819e185d5e61bfd8159
ariariqlubis <ariqf.lubis@gmail.com>
2024-12-18 11:02:15 +0700

    refactor normality_check using shapiro-wilk

--- utils/normality_check.py
+++ utils/normality_check.py
@@ -1,17 +1,16 @@
 import scipy.stats as stats
 from scipy.stats import kstest, norm, anderson
 from statmodels.stats.diagnostic import lilliefors
+from typing import List
 
-def check_normality(data):
+def check_normality(data: List[float], alpha: float = 0.05) -> bool:
+    """
+    Checks if data follows a normal distribution using the Shapiro-Wilk test.
+    Args:
+        data (List[float]): The dataset to check.
+        alpha (float): Significance level for the test.
+    Returns:
+        bool: True if data is normal, False otherwise.
+    """
     stat, p_value_shapiro = stats.shapiro(data)
-
-    if p_value_shapiro > 0.05: # Support H0: Normal Distribution
-        return True
-
-    result_ad = anderson(data)
-    if result_ad.statistic < result_ad.critical_values[2]:
-        return True
-
-    stat_ks, p_value_ks = kstest(data, norm.cdf)
-    if p_value_ks > 0.05:
-        return True
+    return p_value_shapiro > alpha
