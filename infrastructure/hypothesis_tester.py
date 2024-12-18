0a147853143e5f72e34d337d705fbebe45ec5599
ariariqlubis <ariqf.lubis@gmail.com>
2024-12-18 11:01:13 +0700

    Implement HypothesisTester for orchestrating hypothesis testing

--- infrastructure/hypothesis_tester.py
+++ infrastructure/hypothesis_tester.py
@@ -1,25 +1,39 @@
-from core.interfaces.hypothesis_tester import HypothesisTester
-from scipy.stats import ttest_ind, mannwhitneyu
-from utils.normality_check import check_normality
+from core.utils.normality_check import check_normality
+from infrastructure.tests.numerical_test_impl import TTestHypothesis, MannWhitneyUHypothesis
+from infrastructure.tests.categorical_test_impl import ChiSquareTest, FisherExactTest, KruskalWallisTest
+from core.entities.hypothesis import Hypothesis
+from core.entities.test_result import TestResult
 
-class HypothesisTesterImplementation(HypothesisTester):
-    def test(self, hypothesis):
-        if check_normality(hypothesis.data[0]) and check_normality(hypothesis.data[1]):
-            return self.parametric_test(hypothesis)
+class HypothesisTesting:
+    def __init__(self):
+        self.parametric_test  = TTestHypothesis()
+        self.non_parametric_test = MannWhitneyUHypothesis()
+        self.chi_square_test = ChiSquareTest()
+        self.kruskal_test = KruskalWallisTest()
+        self.fisher_test = FisherExactTest()
+        
+        
+    def run_test(self, hypothesis: Hypothesis) -> TestResult:
+        data = hypothesis.data
+        
+        if not data or len(data) < 2:
+            raise ValueError("Hypothesis testing requires at least two groups / datasets")
+        
+        if hypothesis.is_categorical:
+            if len(data) == 2 and all(len(d) == 2 for d in data):
+                return self.fisher_test.test(data)
+            
+            elif len(data) > 2:
+                return self.kruskal_test.test(data)
+            
+            else:
+                return self.chi_square_test.test(data)
+            
         else:
-            return self.non_parametric_test(hypothesis)
-
-
-    def parametric_test(self, hypothesis):
-        stat, p_value = ttest_ind(hypothesis.data[0], hypothesis.data[1])
-        return {
-            "statistic": stat,
-            "p_value": p_value
-        }
-
-    def non_parametric_test(self, hypothesis):
-        stat, p_value = mannwhitneyu(hypothesis.data[0], hypothesis.data[1])
-        return {
-            "statistic": stat,
-            "p_value": p_value
-        }
+            if all(check_normality(d) for d in data):
+                return self.parametric_test.test(data)
+            
+            else:
+                return self.non_parametric_test.test(data)
+        
+        
